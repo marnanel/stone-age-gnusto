f96d5bfeda687bd36ed518859cbc33037b23114c
Thomas Thurman <thomas@thurman.org.uk>
2003-10-14 22:23:58 +0000

    Beret sorts files into the places they should go.

---
+++ src/xpcom/beret/beret.js
@@ -0,0 +1,25 @@
+// -*- Mode: Java; tab-width: 2; -*-
+// $Id: beret.js,v 1.1 2003/10/14 22:23:58 marnanel Exp $
+//
+// Copyright (c) 2003 Thomas Thurman
+// thomas@thurman.org.uk
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have be able to view the GNU General Public License at 
+// http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+const CVS_VERSION = '$Date: 2003/10/14 22:23:58 $';
+const BERET_COMPONENT_ID = Components.ID("{ed0618e3-8b2b-4bc8-b1a8-13ae575efc60}");
+const BERET_DESCRIPTION  = "Checks file magic and routes them accordingly";
+const BERET_CONTRACT_ID  = "@gnusto.org/beret;1";
+
