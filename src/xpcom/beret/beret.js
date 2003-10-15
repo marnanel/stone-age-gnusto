4c0fc25667ba2318928bda9df202ff5adf20bf23
Thomas Thurman <thomas@thurman.org.uk>
2003-10-15 04:56:41 +0000

    First go at the methods.

--- src/xpcom/beret/beret.js
+++ src/xpcom/beret/beret.js
@@ -1,5 +1,5 @@
 // -*- Mode: Java; tab-width: 2; -*-
-// $Id: beret.js,v 1.1 2003/10/14 22:23:58 marnanel Exp $
+// $Id: beret.js,v 1.2 2003/10/15 04:56:41 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -18,8 +18,83 @@
 // http://www.gnu.org/copyleft/gpl.html ; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-const CVS_VERSION = '$Date: 2003/10/14 22:23:58 $';
+////////////////////////////////////////////////////////////////
+
+const CVS_VERSION = '$Date: 2003/10/15 04:56:41 $';
 const BERET_COMPONENT_ID = Components.ID("{ed0618e3-8b2b-4bc8-b1a8-13ae575efc60}");
 const BERET_DESCRIPTION  = "Checks file magic and routes them accordingly";
 const BERET_CONTRACT_ID  = "@gnusto.org/beret;1";
+
+////////////////////////////////////////////////////////////////
+
+function Beret() { }
+
+Beret.prototype = {
+
+		load: function b_load(aLength, info) {
+				dump('(load)\n');
+		},
+
+		get filetype() {
+				return 'none';
+		},
+
+		get engine() {
+				return null;
+		},
+};
+
+////////////////////////////////////////////////////////////////
+//                Standard xpcom inclusion stuff
+////////////////////////////////////////////////////////////////
+
+Factory = new Object();
+
+Factory.createInstance = function f_createinstance(outer, interface_id)
+{
+  if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+
+  if (interface_id.equals(Components.interfaces.gnustoIBeret)) {
+    return new Beret();
+  }
+
+	// otherwise...
+  throw Components.results.NS_ERROR_INVALID_ARG;
+}
+
+////////////////////////////////////////////////////////////////
+
+var Module = new Object();
+
+Module.registerSelf = function m_regself(compMgr, fileSpec, location, type) {
+		reg = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+		reg.registerFactoryLocation(BERET_COMPONENT_ID,
+																BERET_DESCRIPTION,
+																BERET_CONTRACT_ID,
+																fileSpec,
+																location,
+																type);
+}
+
+Module.getClassObject = function m_getclassobj(compMgr, component_id, interface_id) {
+
+		if (component_id.equals(BERET_COMPONENT_ID)) return Factory;
+  
+		// okay, so something's weird. give up.
+		if (interface_id.equals(Components.interfaces.nsIFactory)) {
+				throw Components.results.NS_ERROR_NO_INTERFACE;
+		} else {
+				throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+		}
+}
+
+Module.canUnload = function m_canunload(compMgr) { return true; }
+
+////////////////////////////////////////////////////////////////
+
+function NSGetModule(compMgr, fileSpec) {	return Module; }
+
+////////////////////////////////////////////////////////////////
+
+// EOF beret.js //
 
