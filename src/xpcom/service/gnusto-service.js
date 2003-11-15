45fede59d4fa9b0c06dd814630bddd49c908f28e
Thomas Thurman <thomas@thurman.org.uk>
2003-11-15 22:33:06 +0000

    Bug 4984: Support for a "-gnusto" switch.

--- src/xpcom/service/gnusto-service.js
+++ src/xpcom/service/gnusto-service.js
@@ -1,5 +1,5 @@
 // -*- Mode: Java; tab-width: 2; -*-
-// $Id: gnusto-service.js,v 1.1 2003/11/12 05:23:52 marnanel Exp $
+// $Id: gnusto-service.js,v 1.2 2003/11/15 22:33:06 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -19,8 +19,11 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 ////////////////////////////////////////////////////////////////
+//                Stuff you might want to change:
 
-// Stuff you might want to change:
+// The URL for the main window of Gnusto.
+const GNUSTO_MAIN_WINDOW_URL =
+		'chrome://gnusto/content/';
 
 // A list of MIME types to grab. Each of these will be passed to
 // the beret when Mozilla sees them.
@@ -48,13 +51,72 @@ const mime_types = [
 
 ////////////////////////////////////////////////////////////////
 
-const CVS_VERSION = '$Date: 2003/11/12 05:23:52 $';
+const CVS_VERSION = '$Date: 2003/11/15 22:33:06 $';
 
 const CONTENT_HANDLER_CONTRACT_ID_PREFIX = // Only the start of it:
 		"@mozilla.org/uriloader/content-handler;1?type=";
 const CONTENT_HANDLER_DESCRIPTION_PREFIX = // Only the start of it:
 		"Gnusto's handler for ";
-const CONTENT_HANDLER_COMPONENT_ID = Components.ID("{04f2487c-dad0-4537-8dec-44a818ce9de3}");
+const CONTENT_HANDLER_COMPONENT_ID =
+		Components.ID("{04f2487c-dad0-4537-8dec-44a818ce9de3}");
+
+const COMMAND_LINE_CONTRACT_ID =
+		'@mozilla.org/commandlinehandler/general-startup;1?type=gnusto';
+const COMMAND_LINE_COMPONENT_ID =
+		Components.ID("{328a6b88-ee6e-41fe-828c-8c71b807b46a}");
+const COMMAND_LINE_DESCRIPTION = 'Gnusto command-line service';
+const COMMAND_LINE_CATEGORY = 'command-line-argument-handlers';
+
+////////////////////////////////////////////////////////////////
+
+// Our command-line switch is "-gnusto". The parameter (which
+// defaults to "-") is passed through to the window.
+
+function CommandLineService() {}
+CommandLineService.prototype.commandLineArgument = '-gnusto';
+CommandLineService.prototype.prefNameForStartup =
+		'general.startup.gnusto';
+CommandLineService.prototype.chromeUrlForTask =
+		GNUSTO_MAIN_WINDOW_URL;
+CommandLineService.prototype.helpText =
+		'Play interactive fiction games.';
+// Where can I find documentation for the following? I'm having
+// to do this by trial and error :/
+CommandLineService.prototype.handlesArgs = true;
+CommandLineService.prototype.defaultArgs = "-";
+CommandLineService.prototype.openWindowWithArgs = true;
+
+////////////////////////////////////////////////////////////////
+
+var CommandLineFactory = new Object();
+CommandLineFactory.createInstance = function clf_createInstance(outer, iid) {
+		if (outer!=null) {
+				throw Components.results.NS_ERROR_NO_AGGREGATION;
+		}
+
+		if (!iid.equals(Components.interfaces.nsICmdLineHandler) && !iid.equals(Components.interfaces.nsISupports)) {
+				throw Components.results.NS_ERROR_INVALID_ARG;
+		}
+
+		return new CommandLineService;
+}
+
+////////////////////////////////////////////////////////////////
+
+function openInNewWindow(url) {
+		try {
+				var ass = Components.classes['@mozilla.org/appshell/appShellService;1'].getService(Components.interfaces.nsIAppShellService);
+				ass.hiddenDOMWindow.open(url,
+																 '_blank',
+																 'chrome,all,dialog=no');
+				
+		} catch (e) {
+				// FIXME: we want some proper error handling
+				dump('openInNewWindow: FAILED: ');
+				dump(e);
+				dump('\n');
+		}
+}
 
 ////////////////////////////////////////////////////////////////
 //                       CONTENTHANDLER CLASS
@@ -79,20 +141,7 @@ ContentHandler.prototype.handleContent = function ch_hc(contentType,
 																												windowTarget,
 																												request)
 {
-		dump('--- handlecontent ');
-
-		try {
-				var ass = Components.classes['@mozilla.org/appshell/appShellService;1'].getService(Components.interfaces.nsIAppShellService);
-				ass.hiddenDOMWindow.open('chrome://gnusto/content/',
-																 '_blank',
-																 'chrome,all,dialog=no');
-				
-				dump('DONE.\n');
-		} catch (e) {
-				dump('FAILED: ');
-				dump(e);
-				dump('\n');
-		}
+		openInNewWindow(GNUSTO_MAIN_WINDOW);
 }
 
 
@@ -122,21 +171,30 @@ var Module = new Object();
 
 Module.registerSelf = function m_regself(compMgr, fileSpec, location, type) {
 		try {
-				reg = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+
+				var reg = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+				var catman = Components.classes['@mozilla.org/categorymanager;1'].getService(Components.interfaces.nsICategoryManager);
+
+				reg.registerFactoryLocation(COMMAND_LINE_COMPONENT_ID,
+																		COMMAND_LINE_DESCRIPTION,
+																		COMMAND_LINE_CONTRACT_ID,
+																		fileSpec, location, type);
+
+				catman.addCategoryEntry(COMMAND_LINE_CATEGORY,
+																COMMAND_LINE_DESCRIPTION,
+																COMMAND_LINE_CONTRACT_ID,
+																true, true);
+																
+
 				for (i in mime_types) {
 						var mime_type = mime_types[i];
-						dump('registering ');
-						dump(mime_type);
-						dump('... ');
 						reg.registerFactoryLocation(CONTENT_HANDLER_COMPONENT_ID,
 																				CONTENT_HANDLER_DESCRIPTION_PREFIX + mime_type,
 																				CONTENT_HANDLER_CONTRACT_ID_PREFIX + mime_type,
 																				fileSpec,
 																				location,
 																				type);
-						dump('done.\n');
 				}
-				dump('all registering done.\n');
 		} catch (e) {
 				dump('FAIL: ');
 				dump(e);
@@ -174,6 +232,10 @@ Module.getClassObject = function m_getclassobj(compMgr,
 				return ContentHandlerFactory;
 		}
   
+		if (component_id.equals(COMMAND_LINE_COMPONENT_ID)) {
+				return CommandLineFactory;
+		}
+
 		// okay, so something's weird. give up.
 		if (interface_id.equals(Components.interfaces.nsIFactory)) {
 				throw Components.results.NS_ERROR_NO_INTERFACE;
