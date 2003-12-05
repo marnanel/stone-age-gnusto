bf768c4af3c9380b068666ac9374572e17ce3368
Thomas Thurman <thomas@thurman.org.uk>
2003-12-05 16:34:44 +0000

    Takes error details from the error list, just like it used to.

--- src/xpcom/errorbox/gnusto-errorbox.js
+++ src/xpcom/errorbox/gnusto-errorbox.js
@@ -1,5 +1,5 @@
 // Gnusto: error dialogue control. -*- Mode: Java; tab-width: 2; -*-
-// $Id: gnusto-errorbox.js,v 1.1 2003/12/05 08:38:29 marnanel Exp $
+// $Id: gnusto-errorbox.js,v 1.2 2003/12/05 16:34:44 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -20,7 +20,7 @@
 
 ////////////////////////////////////////////////////////////////
 
-const CVS_VERSION = '$Date: 2003/12/05 08:38:29 $';
+const CVS_VERSION = '$Date: 2003/12/05 16:34:44 $';
 const ERRORBOX_COMPONENT_ID = Components.ID("{ec3597d6-e302-437c-aeef-9880448dedca}");
 const ERRORBOX_DESCRIPTION  = "Displays error messages and does logging";
 const ERRORBOX_CONTRACT_ID  = "@gnusto.org/errorbox;1";
@@ -29,126 +29,88 @@ const ERRORBOX_XUL = "chrome://gnusto/content/errorDialog.xul";
 const ERRORBOX_TITLE = "error";
 const ERRORBOX_OPTIONS = "modal,centerscreen,chrome,resizable=no";
 
+// The file containing all the error messages and defaults.
+// Because this is in ".../locale/...", Moz will route it to
+// the correct locale for the job.
+const PROPERTIES_FILE = "chrome://gnusto/locale/gnusto.properties";
+
+////////////////////////////////////////////////////////////////
+
+var errorbook = 0;
+
+function load_errorbook() {
+		if (errorbook != 0) return;
+
+		var bundle = Components.classes['@mozilla.org/intl/stringbundle;1'].getService(Components.interfaces.nsIStringBundleService).createBundle(PROPERTIES_FILE);
+
+		errorbook = {};
+
+		var enumeration = bundle.getSimpleEnumeration();
+		while (enumeration.hasMoreElements()) {
+				var prop = enumeration.getNext().QueryInterface(Components.interfaces.nsIPropertyElement);
+
+				key = prop.key.split('.');
+				value = prop.value;
+
+				if (key[0]=='error' && key.length>=3) {
+						if (!(key[1] in errorbook)) {
+								errorbook[key[1]] = {};
+						}
+
+						errorbook[key[1]][key[2]] = value;
+				}
+		}
+
+}
 
 ////////////////////////////////////////////////////////////////
 
 function ErrorBox() { }
 
 ErrorBox.prototype = {
-
-//The original gnusto_error:
-//
-//function gnusto_error(n) {
-//		
-//		//VERBOSE burin('ERROR', n);
-//		if (ignore_errors[n])
-//				return;
-//
-//		var m = getMsg('error.'+n+'.name', arguments, 'Unknown error!');
-//
-//		m = m + '\n\n' + getMsg('error.'+n+'.details', arguments, '');
-//
-//		m = m + '\n\nError #'+n+'-- ';
-//
-//		if (n>=500)
-//				m = m + 'transient';
-//		else
-//				m = m + 'fatal';
-//
-//		for (var i=1; i<arguments.length; i++) {
-//				if (arguments[i] && arguments[i].toString) {
-//						m = m + '\nDetail: '+arguments[i].toString();
-//				}
-//		}
-//
-//		var procs = arguments.callee;
-//		var procstring = '';
-//
-//		var loop_count = 0;
-//		var loop_max = 100;
-//
-//		while (procs!=null && loop_count<loop_max) {
-//				var name = procs.toString();
-//
-//				if (name==null) {
-//						procstring = ' (anon)'+procstring;
-//				} else {
-//						var r = name.match(/function (\w*)/);
-//
-//						if (r==null) {
-//								procstring = ' (weird)' + procstring;
-//						} else {
-//								procstring = ' ' + r[1] + procstring;
-//						}
-//				}
-//
-//				procs = procs.caller;
-//				loop_count++;
-//		}
-//
-//		if (loop_count==loop_max) {
-//				procstring = '...' + procstring;
-//		}
-//
-//		m = m + '\n\nJS call stack:' + procstring;
-//
-//		m = m + '\n\nZ call stack:';
-//
-//		try {
-//				for (var i in call_stack) {
-//						// We don't have any way of finding out the real names
-//						// of z-functions at present. This will have to do.
-//						m = m + ' ('+call_stack[i].toString(16)+')'
-//				}
-//
-//				if (pc!=null)
-//						m = m + '\nProgram counter: '+pc.toString(16);
-//
-//				m = m + '\nZ eval stack (decimal):';
-//				for (var i in gamestack) {
-//						m = m + ' '+ gamestack[i];
-//				}
-//
-//				if (locals!=null) {
-//						m = m + '\nLocals (decimal):';
-//						for (var i=0; i<16; i++) {
-//								m = m + ' ' + i + '=' + locals[i];
-//						}
-//				}
-//
-//				if (debug_mode) {
-//						glue_print('\n\n--- Error ---:\n'+m);
-//				}
-//
-//		} catch (e) {
-//				m = m + '(Some symbols not defined.)';
-//		}
-//		
-//		if (!ignore_transient_errors) {
-//                  window.openDialog("chrome://gnusto/content/errorDialog.xul", "Error", "modal,centerscreen,chrome,resizable=no", m, n);               
-//                }
-//
-//		if (n<500) throw -1;
-//}
-
 		alert: function eb_alert(code, details) {
 
 				if (this.m_ignore_transients && (code < 500)) {
 						return;
 				}
+			
+				var message = '';
 
 				try {
-						var message = code.toString()+' - '+details;
+						load_errorbook();
+
+						// Prepare a message.
+
+						message = 'Error #'+code.toString()+' ';
+
+						if (message<500) {
+								message += '(transient)';
+						} else {
+								message += '(fatal)';
+						}
+
+						message += '\n\n';
+
+						if (code in errorbook) {
+								message += errorbook[code].name+
+								           '\n\n'+
+								           errorbook[code].details;
+						} else {
+								message += 'Unknown error.';
+						}
+
+						message += '\n\nDetails:\n' + details;
+
+						// Put up the window.
 
 						var watcher = Components.classes['@mozilla.org/embedcomp/window-watcher;1'].getService(Components.interfaces.nsIWindowWatcher);
 						
 						// FIXME: The parent should be the Gnusto main window, if possible.
 
 						var args = Components.classes['@mozilla.org/embedcomp/dialogparam;1'].getService(Components.interfaces.nsIDialogParamBlock);
-						dump(args);
 						args.SetInt(0, code);
 						args.SetString(0, message);
-
+			
 						// Because this is a dialogue, execution pauses here until the
 						// user dismisses it.
 						
@@ -161,6 +123,8 @@ ErrorBox.prototype = {
 				} catch(e) {
 						dump('alert error: ');
 						dump(e);
+						dump('\n');
+						dump(message);
 						dump('\n');
 				}
 		},
@@ -178,6 +142,7 @@ ErrorBox.prototype = {
 		},
 
 		m_ignore_transients: 0,
+
 };
 
 ////////////////////////////////////////////////////////////////
