a88abbc6b95eddf0178c781b7bbce63172b4c5e2
Thomas Thurman <thomas@thurman.org.uk>
2004-01-29 07:03:52 +0000

    first draft

---
+++ src/tests/gamescripts/stdspells.h
@@ -0,0 +1,12 @@
+GNUSTO.MAGIC.GRIMOIRE=1
+#
+# The really basic settings which almost every grimoire needs.
+# Include these using
+#
+#   open=stdspells.h
+#
+
+set.seed=1234
+set.nowin=1
+set.gameoverquit=1      # Currently the only way to stop; see bug 5657
+set.output=/dev/stdout
