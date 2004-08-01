b126cb54c9430e5ab3916ba4553fb56b19a30ab9
Thomas Thurman <thomas@thurman.org.uk>
2004-08-01 03:18:32 +0000

    Should have the same signature as before! Fixed.

--- src/gnusto/content/gnusto-base.js
+++ src/gnusto/content/gnusto-base.js
@@ -1,4 +1,4 @@
-// $Header: /cvs/gnusto/src/gnusto/content/gnusto-base.js,v 1.2 2004/08/01 02:53:18 marnanel Exp $
+// $Header: /cvs/gnusto/src/gnusto/content/gnusto-base.js,v 1.3 2004/08/01 03:18:32 marnanel Exp $
 //
 // Copyright (c) 2003 Thomas Thurman
 // thomas@thurman.org.uk
@@ -35,28 +35,43 @@
 //
 // Notable points of entry in this file:
 //
-//   - command_open() -- nyi
+//   - command_open() -- load a file
 //
 ////////////////////////////////////////////////////////////////
 
-function command_open(nolaunch) {
+function command_open(filename, nolaunch) {
 
-  var ifp = Components.interfaces.nsIFilePicker;
-  var picker = Components.classes["@mozilla.org/filepicker;1"].
-    createInstance(ifp);
+  var localfile;
 
-  // FIXME: This list should be made from components;
-  // it shouldn't be hard-coded like this.
-  picker.init(window, "Select a story file", ifp.modeOpen);
-  picker.appendFilter("Z-code", "*.z?");
-  picker.appendFilter("Blorb", "*.blb");
-  picker.appendFilter("Glulx", "*.ulx");
-  picker.appendFilter("Saved game", "*.sav; *.qtz");
-  picker.appendFilter("Play-testing script", "*.grimoire");
-				
-  if (picker.show()!=ifp.returnOK) return 0;
-						
-  load_from_file(picker.file);
+  if (filename) {
+
+    localfile = new Components.Constructor("@mozilla.org/file/local;1",
+					   "nsILocalFile",
+					   "initWithPath")(filename);
+    filename = filename.replace('\\','\\\\', 'g');
+
+  } else {
+
+    var ifp = Components.interfaces.nsIFilePicker;
+    var picker = Components.classes["@mozilla.org/filepicker;1"].
+      createInstance(ifp);
+
+    // FIXME: This list should be made from components;
+    // it shouldn't be hard-coded like this.
+    picker.init(window, "Select a story file", ifp.modeOpen);
+    picker.appendFilter("Z-code", "*.z?");
+    picker.appendFilter("Blorb", "*.blb");
+    picker.appendFilter("Glulx", "*.ulx");
+    picker.appendFilter("Saved game", "*.sav; *.qtz");
+    picker.appendFilter("Play-testing script", "*.grimoire");
+
+    if (picker.show()!=ifp.returnOK) return 0;
+
+    localfile = picker.file;
+    filename = localfile.path;
+  }
+
+  load_from_file(localfile);
 
   if (!nolaunch) {
     glue_play();
@@ -66,8 +81,6 @@ function command_open(nolaunch) {
 
   // FIXME: At one point we had these lines; something similar
   // should eventually be re-included here.
-  // filename = picker.file.path;
-  // filename = filename.replace('\\','\\\\', 'g');
   //if (filename && result==1) {
   //		sys_notify_of_load(filename);
   //		sys_show_story_title(filename);
@@ -77,3 +90,5 @@ function command_open(nolaunch) {
 ////////////////////////////////////////////////////////////////
 var GNUSTO_BASE_HAPPY = 1;
 ////////////////////////////////////////////////////////////////
+
+
