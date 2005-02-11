d8b0994851d96eab3fb51cef95fd8bb8272e2003
Eric Liga <naltrexone42@yahoo.com>
2005-02-11 10:10:08 +0000

    First pass at registering Gnusto as a Helper App. This works on my machine, but will NOT work on anyone else's yet.  Had to hard-code the path to firefox.exe.  When we can get that via script, we're good to go.

--- src/gnusto/content/gnustooverlay.js
+++ src/gnusto/content/gnustooverlay.js
@@ -23,3 +23,72 @@ function gnustoButtonPress()
 {
   document.getElementById("gnustopopup2").showPopup(document.getElementById("gnusto-button"),-1, -1,"popup","bottomleft","topleft");	
 }
+
+var gDS = null;
+var gRDF = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);     
+
+function registerHelperApp() {
+       Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader).loadSubScript("chrome://gnusto/content/overrideHandler.js");
+       
+       const mimeTypes = "UMimTyp";
+       var fileLocator = Components.classes["@mozilla.org/file/directory_service;1"].getService();
+       if (fileLocator)
+         fileLocator = fileLocator.QueryInterface(Components.interfaces.nsIProperties);
+       var file = fileLocator.get(mimeTypes, Components.interfaces.nsIFile);
+       var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+       var fileHandler = ioService.getProtocolHandler("file").QueryInterface(Components.interfaces.nsIFileProtocolHandler);
+       gDS = gRDF.GetDataSource(fileHandler.getURLSpecFromFile(file));
+       
+       // Get mime type from which we can construct a HandlerInfo...
+       var mimeType = 'application/x-zmachine';
+       
+       // Create HandlerOverride and populate it.
+       var entry = new HandlerOverride(MIME_URI(mimeType));
+       var valueProperty = gRDF.GetUnicodeResource(NC_RDF("value"));
+       var mimeSource = gRDF.GetUnicodeResource(MIME_URI(mimeType));
+       var mimeLiteral = gRDF.GetLiteral(mimeType);
+       entry.mUpdateMode = gDS.HasAssertion(mimeSource, valueProperty, mimeLiteral, true);
+       entry.mimeType    = mimeType;
+       entry.isEditable  = true;
+       entry.alwaysAsk   = false;
+       
+       // If not updating (i.e., a newly encountered mime type),
+       // then update extension list and description.
+       if (!entry.mUpdateMode) {
+         entry.addExtension('z1');
+         entry.addExtension('z2');
+         entry.addExtension('z3');
+         entry.addExtension('z4');
+         entry.addExtension('z5');
+//         entry.addExtension('z6');  --reenable when we add z6 support
+         entry.addExtension('z7');
+         entry.addExtension('z8');
+         entry.description = 'Interactive Fiction';
+         entry.appDisplayName = 'Gnusto';         
+       }
+       
+       entry.saveToDisk       = false;
+       entry.useSystemDefault = false;
+       entry.handleInternal   = false;
+       entry.appPath = 'c:\\program files\\mozilla firefox\\firefox.exe';
+
+
+       
+       // Do RDF magic.
+       try {
+         entry.buildLinks();
+       } catch (e) {
+         // ignore errors	
+       }
+       
+       // flush the ds to disk.
+       var remoteDS = gDS.QueryInterface(Components.interfaces.nsIRDFRemoteDataSource);
+       if (remoteDS)
+         remoteDS.Flush();   
+  
+}
+try {
+  registerHelperApp();
+} catch (e) {
+	//ignore errors
+}
